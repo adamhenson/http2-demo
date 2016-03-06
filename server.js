6c981751d33c7c7916fb797675f7d8abbb0214cf
Adam Henson <adamhenson1979@gmail.com>
2016-03-06 14:18:19 -0500

    Expire time.

--- server.js
+++ server.js
@@ -6,24 +6,45 @@ var http2 = require('http2');
 var path = require('path');
 var Template = require('./templates/MainTemplate');
 
+var today = new Date();
+var now = today.toUTCString();
+var cacheExpire = today;
+cacheExpire.setMinutes(today.getMinutes() + 5);
+cacheExpire = cacheExpire.toUTCString();
+
 // File push queue
 const FILES = [
-  'path' : '/public/css/main.css',
-  'path' : '/public/images/nyc.jpg'
+  {
+    'headers' : {
+      'etag' : new Date().getTime().toString() + '1',
+      'content-type' : 'text/css',
+      'cache-control' : 'no-transform',
+      'date' : now,
+      'expires' : cacheExpire,
+      'last-modified' : 'Sat, 01 Aug 2015 00:45:10 GMT'
+    },
+    'path' : '/public/css/main.css'
+  },
+  {
+    'headers' : {
+      'etag' : new Date().getTime().toString() + '2',
+      'content-type' : 'image/jpeg',
+      'cache-control' : 'no-transform',
+      'date' : now,
+      'expires' : cacheExpire,
+      'last-modified' : 'Sat, 01 Aug 2015 00:45:09 GMT'
+    },
+    'path' : '/public/images/nyc.jpg'
+  }
 ];
-
-// Create read stream and add to response
-function createReadStreamResponse(file, response) {
-  let push = response.push(file);
-  push.writeHead(200);
-  fs.createReadStream(path.join(__dirname, file)).pipe(push);
-}
 
 // Request callback
 function onRequest(request, response) {
   if(response.push) {
     FILES.forEach((file, index) => {
-      createReadStreamResponse(file, response);
+      let push = response.push(file.path);
+      push.writeHead(200, file.headers);
+      fs.createReadStream(path.join(__dirname, file.path)).pipe(push);
       if(index === FILES.length - 1) response.end(Template.output(FILES));
     });
   }

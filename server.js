b0534012002dc1fc0f66975d3724416c21c8a850
adamhenson <adamhenson1979@gmail.com>
2016-07-14 17:23:27 -0400

    Middleware push POC.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 const APP_INFO = require('./package');
 const fs = require('fs');
+const FILES = require('./constants/files');
 const getHTML = require('./templates/MainTemplate');
 const http2 = require('http2');
 const log = require('./lib/util').createLogger('server');
 const path = require('path');
 const url = require('url');
+const Pusher = require('./middleware/Pusher');
 
 // Cannot use Express until it supports http2 server push,
 // therefore simply using the routing Express uses until
@@ -24,108 +26,13 @@ function app(req, res) {
   router(req, res, finalhandler(req, res));
 }
 
-// File push queue
-const FILES = [
-  {
-    'headers' : {
-      'content-type' : 'text/css'
-    },
-    'path' : '/public/css/main.css'
-  },
-  {
-    'headers' : {
-      'content-type' : 'application/javascript'
-    },
-    'path' : '/public/js/main.js'
-  },
-  {
-    'headers' : {
-      'content-type' : 'image/jpeg'
-    },
-    'path' : '/public/images/nyc.jpg'
-  },
-  {
-    'headers' : {
-      'content-type' : 'image/x-icon'
-    },
-    'path' : '/public/images/favicon.ico'
-  }
-];
+let pusher = new Pusher();
 
-// Error response for http/1.1 fallback
-function errorResponse(response, error) {
-  response.writeHead(500, { 'content-type' : 'text/plain' });
-  response.write(`Error: ${error}`);
-  response.end();
-}
-
-// Failure response for http/1.1 fallback
-function failResponse(response) {
-  response.writeHead(404, { 'content-type' : 'text/plain' });
-  response.write('404 Not Found');
-  response.end();
-}
-
-// Fallback static file server for http/1.1 fallback
-function createStaticFileServer(request, response) {
-  let uri = url.parse(request.url).pathname
-  let filename = path.join(process.cwd(), uri);
-
-  // if there is a matched file, this will return an array
-  // with one result.
-  let matchedFileObjectArray = FILES.filter((fileObject) => {
-    return fileObject.path === uri;
+router.get('/', pusher.push, function (req, res) {
+  let html = getHTML({
+    'title' : 'Hello HTTP/2'
   });
-
-  if(uri === '/') {
-    let html = getHTML({
-      'title' : 'Hello HTTP/1.1'
-    });
-
-    response.end(html);
-  } else if(!matchedFileObjectArray.length) {
-    failResponse(response);
-  } else {
-    let fileConfig = matchedFileObjectArray[0];
-
-    fs.exists(filename, (exists) => {
-      if(!exists) {
-        failResponse(response);
-        return;
-      }
-
-      fs.readFile(filename, (err, file) => {
-        if(err) {        
-          errorResponse(response, err);
-          return;
-        }
-
-        response.writeHead(200, fileConfig.headers);
-        response.write(file);
-        response.end();
-      });
-    });
-  }
-}
-
-router.get('/', function (req, res) {
-  if(res.push) {
-    FILES.forEach((file, index) => {
-      let push = res.push(file.path);
-      push.writeHead(200, file.headers);
-
-      fs.createReadStream(path.join(__dirname, file.path)).pipe(push);
-
-      if(index === FILES.length - 1) {
-        let html = getHTML({
-          'title' : 'Hello HTTP/2'
-        });
-        res.end(html);
-      }
-    });
-  } else {
-    createStaticFileServer(req, res);
-  }
+  res.end(html);
 })
 
 // Server
